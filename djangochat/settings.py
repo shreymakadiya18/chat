353029beb720308c89d2f59f79d76f6d539ec838
shreymakadiya18 <86375553+shreymakadiya18@users.noreply.github.com>
2022-10-20 09:42:58 +0530

    Update settings.py

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm%c30v(4=6q!_ouzf5xx9l7n89pl0#l16lo6+z*g4!49-63@t#'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['shrey-18.herokuapp.com','127.0.0.1:8000']
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
